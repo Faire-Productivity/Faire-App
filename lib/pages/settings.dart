f741cc4be09393e75664ae8735a2d64aa6899512
Zyril Del Rosario <mitski.zyril@gmail.com>
2020-11-25 04:00:20 -0800

    Added more options in settings
    Also added one of the following:
    
    url_launcher
    shared_preferences
    firebase_analytics

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -6,22 +7,81 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  final Uri _faireGitHubUri = Uri.https('github.com', '/Faire-Productivity/Faire-App/issues');
+  bool _lyticsEnabled = false;
+  bool _darkModeEnabled = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
       ),
-      body: ListView(
+      body: Column(
         children: [
           ListTile(
-            title: Text("About", style: TextStyle(fontWeight: FontWeight.bold),),
-            onTap: () {
-              showAbout();
-            },
-          )
+            title: Text('Your Data', style: TextStyle(color: Colors.pink, fontSize: 14, fontWeight: FontWeight.bold),),
+            contentPadding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+          ),
+          ListView(
+            shrinkWrap: true,
+            children: [
+              SwitchListTile(
+                title: Text("Enable Analytics", style: TextStyle(fontWeight: FontWeight.bold),),
+                secondary: Icon(Icons.analytics),
+                value: _lyticsEnabled,
+                onChanged: (bool value) {
+                  setState(() {
+                    _lyticsEnabled = !_lyticsEnabled;
+                  });
+                },
+              ),
+            ],
+          ),
+          ListTile(
+            title: Text('Experimental', style: TextStyle(color: Colors.pink, fontSize: 14, fontWeight: FontWeight.bold),),
+            contentPadding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+          ),
+          ListView(
+            shrinkWrap: true,
+            children: [
+              SwitchListTile(
+                title: Text("Dark Mode", style: TextStyle(fontWeight: FontWeight.bold),),
+                secondary: Icon(Icons.nights_stay),
+                value: _darkModeEnabled,
+                onChanged: (bool value) {
+                  setState(() {
+                    _darkModeEnabled = !_darkModeEnabled;
+                  });
+                },
+              ),
+            ],
+          ),
+          ListTile(
+            title: Text('Other', style: TextStyle(color: Colors.pink, fontSize: 14, fontWeight: FontWeight.bold),),
+            contentPadding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+          ),
+          ListView(
+            shrinkWrap: true,
+            children: [
+              ListTile(
+                title: Text("Report an Issue", style: TextStyle(fontWeight: FontWeight.bold),),
+                onTap: () {
+                  launch(_faireGitHubUri.toString());
+                },
+                leading: Icon(Icons.bug_report),
+              ),
+              ListTile(
+                title: Text("About", style: TextStyle(fontWeight: FontWeight.bold),),
+                onTap: () {
+                  showAbout();
+                },
+                leading: Icon(Icons.info),
+              )
+            ],
+          ),
         ],
-      ),
+      )
     );
   }
 
@@ -29,7 +89,7 @@ class _SettingsPageState extends State<SettingsPage> {
     showAboutDialog(
       context: context,
       applicationName: "Faire",
-      applicationVersion: "0.1.1-Github",
+      applicationVersion: "0.1.2",
       applicationIcon: Image.asset("assets/faire_favicon.png",height: 50,),
       children: [
         Text("Faire, a new way to be productive.")
